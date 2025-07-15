2f54d047320fae77bfd360bb3db88d605cd0cb2c
Zeze(강재영) <wooder2050@gmail.com>
2025-07-15 22:53:40 +0900

    feat: add season summary table component and refactor season summary logic

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['picsum.photos'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
